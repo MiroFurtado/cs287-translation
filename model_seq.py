beaeac30fefcac090afd9ac24a529f7e1af13c2c
Miro Furtado <furtadomiro@gmail.com>
2019-03-03 13:08:49 -0500

    .

--- model_seq.py
+++ model_seq.py
@@ -2,10 +2,10 @@ import torch
 from namedtensor import ntorch, NamedTensor
 
 class EncoderS2S(ntorch.nn.Module):
-  def __init__(self, hidden_dim = 512, num_layers = 2, dropout=0.5):
+  def __init__(self, hidden_dim = 512, num_layers = 2, dropout=0.5, src_vocab_len = 13353):
     super(EncoderS2S, self).__init__()
     
-    self.embedding = ntorch.nn.Embedding(len(DE.vocab), hidden_dim).spec("srcSeqlen", "embedding")
+    self.embedding = ntorch.nn.Embedding(src_vocab_len, hidden_dim).spec("srcSeqlen", "embedding")
     self.dropout = ntorch.nn.Dropout(dropout)
     self.LSTM = ntorch.nn.LSTM(hidden_dim, hidden_dim, num_layers, dropout=dropout).spec("embedding", "srcSeqlen", "hidden")
   
@@ -18,10 +18,10 @@ class EncoderS2S(ntorch.nn.Module):
     return x, hidden
 
 class DecoderS2S(ntorch.nn.Module):
-  def __init__(self, hidden_dim = 512, num_layers = 2, dropout = 0.5):
+  def __init__(self, hidden_dim = 512, num_layers = 2, dropout = 0.5, trg_vocab_len = 11560):
     super(DecoderS2S, self).__init__()
     
-    self.embedding = ntorch.nn.Embedding(len(EN.vocab), hidden_dim).spec("trgSeqlen", "embedding")
+    self.embedding = ntorch.nn.Embedding(trg_vocab_len, hidden_dim).spec("trgSeqlen", "embedding")
     self.dropout = ntorch.nn.Dropout(dropout)
     self.LSTM = ntorch.nn.LSTM(hidden_dim, hidden_dim, num_layers, dropout=dropout).spec("embedding", "trgSeqlen", "hidden")
     self.out = ntorch.nn.Linear(hidden_dim, len(EN.vocab)).spec("hidden", "vocab")
